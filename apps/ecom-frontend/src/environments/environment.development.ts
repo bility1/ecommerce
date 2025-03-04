0a92f6ac5b7ed863e3f56d29a78307955215609f
Bility1 <lanfiab13.mk@gmail.com>
2025-03-04 11:34:28 +0100

    commande

--- apps/ecom-frontend/src/environments/environment.development.ts
+++ apps/ecom-frontend/src/environments/environment.development.ts
@@ -5,6 +5,6 @@ export const environment = {
     postLogoutRedirectUri:"http://localhost:4200",
     cliendId: "c95d314ac2764cf9a5920b2cd968da87",
     audience:"https://soudou.kinde.com/api",
-    }
+    },
     apiUrl:"http://localhost:8080/api",
   }
--- apps/ecom-frontend/src/environments/environment.ts
+++ apps/ecom-frontend/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   kinde :{
-    authority: "https://soudou.kinde.com",
-    redirectUrl: "http://localhost:4200",
-    postLogoutRedirectUri:"http://localhost:4200",
-    cliendId: "c95d314ac2764cf9a5920b2cd968da87",
-    audience:"https://soudou.kinde.com/api",
-    }
-    apiUrl:"http://localhost:8080/api",
+    authority: 'https://soudou.kinde.com',
+    redirectUrl: 'http://localhost:4200',
+    postLogoutRedirectUri:'http://localhost:4200',
+    cliendId: 'c95d314ac2764cf9a5920b2cd968da87',
+    audience:'http://localhost:8080/api',
+    },
+    apiUrl:'http://localhost:8080/api',
   }
